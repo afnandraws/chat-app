ac1b7a890ecfe4b3621e4387cc16abca91bfd466
Afnan Khan <40701226+afnandraws@users.noreply.github.com>
2023-09-17 00:53:09 +0100

    working

--- frontend/components/SocketContainer.jsx
+++ frontend/components/SocketContainer.jsx
@@ -44,7 +44,7 @@ const SocketContainer = () => {
         socket?.on('initial_connection', (id) => {userID = id; console.log(userID)})
 
         if (socket === null) {
-            setSocket(io('http://localhost:8080', { autoConnect: false }))
+            setSocket(io('https://chat-app-r3il.onrender.com', { autoConnect: false }))
         }
 
         return () => {
