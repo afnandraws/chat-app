88e1b8c1eb377ac75a5a6f1330bf192e243bbfb3
Afnan Khan <40701226+afnandraws@users.noreply.github.com>
2023-10-14 14:32:47 +0100

    adjusting colours

--- frontend/components/NewChat.jsx
+++ frontend/components/NewChat.jsx
@@ -3,9 +3,8 @@ import styles from './NewChat.module.css'
 const NewChat = ({ createRoomHandler, joinRoomHandler, roomNumberRef }) => {
     return (
         <div className={styles.newchat}>
-        <button onClick={() => {joinRoomHandler(roomNumberRef)}}>join a room</button>
+        <button style= {{backgroundColor: '#fbfcff', color: '#567AF4'}} onClick={() => {joinRoomHandler(roomNumberRef)}}>join a room</button>
         <button onClick={(event) => {createRoomHandler(event)}}>create room</button>
-        {/* <input onKeyDown={(event) => {createRoomHandler(event)}} type="text" placeholder="Or type to start a new chat!" /> */}
         </div>
     );
 }
